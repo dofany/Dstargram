db7b4a20b223f0a6cf6bd36b4ec27e7ea02ee60d
dofany <kimdohwan17@gmail.com>
2021-05-29 20:03:22 +0900

    dstargram_clone

---
+++ photo/urls.py
@@ -0,0 +1,15 @@
+from django.urls import path
+from .views import PhotoList, PhotoCreate, PhotoDelete, PhotoDetail, PhotoUpdate
+
+app_name = "photo"
+urlpatterns = [
+    path("create/", PhotoCreate.as_view(), name='create'),
+    path("detail/<int:pk>/", PhotoDetail.as_view(), name='detail'),
+    path("delete/<int:pk>/", PhotoDelete.as_view(), name='delete'),
+    path("update/<int:pk>/", PhotoUpdate.as_view(), name='update'),
+    path("", PhotoList.as_view(), name='index'),
+]
+
+from django.conf.urls.static import static
+from django.conf import settings
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
---
+++ photo/views.py
@@ -0,0 +1,38 @@
+from django.shortcuts import render, redirect
+from django.views.generic.list import ListView
+from django.views.generic.edit import CreateView,UpdateView, DeleteView
+from django.views.generic.detail import DetailView
+from .models import Photo
+
+class PhotoList(ListView):
+    model = Photo
+    template_name_suffix = '_list'
+
+class PhotoCreate(CreateView):
+    model = Photo
+    fields = ['author','text','image']
+    template_name_suffix = '_create'
+    success_url = '/'
+
+    def form_valid(self, form):
+        form.instance.author_id = self.request.user.id
+        if form.is_valid():
+            form.instance.save()
+            return redirect('/')
+        else:
+            return self.render_to_response({'form':form})
+
+class PhotoUpdate(UpdateView):
+    model = Photo
+    fields = ['author','text', 'image']
+    template_name_suffix = '_update'
+    success_url = '/'
+
+class PhotoDelete(DeleteView):
+    model = Photo
+    template_name_suffix = '_delete'
+    success_url = '/'
+
+class PhotoDetail(DetailView):
+    model = Photo
+    template_name_suffix = '_detail'
